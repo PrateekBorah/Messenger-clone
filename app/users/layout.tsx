a28bf7e91d0f2b1d71937de54696e070f30c157f
Prateek Borah <prateekborah@Prateeks-Laptop.local>
2023-10-21 12:32:42 +0530

    First Commit

---
+++ app/users/layout.tsx
@@ -0,0 +1,19 @@
+import getUsers from "../actions/getUsers";
+import UserList from "./components/UserList";
+import Sidebar from "../components/sidebar/Sidebar";
+
+const UsersLayout = async ({children}:{children:React.ReactNode})=>{
+
+    const users = await getUsers();
+
+    return(
+        <Sidebar>
+            <div className="h-full">
+                <UserList items={users}/>
+                {children}
+            </div>
+        </Sidebar>
+    )
+}
+
+export default UsersLayout;
